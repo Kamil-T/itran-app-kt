1f65227092653a8be63ff764e6ed28bca6cf989e
Kamil-T <trzaskomakamil@gmail.com>
2022-11-02 02:20:00 +0100

    All Collection List working

--- src/pages/collections/index.tsx
+++ src/pages/collections/index.tsx
@@ -1,7 +1,25 @@
+import type { Collection } from "@prisma/client";
+import prisma from "../../../lib/prisma";
+
+import { GetServerSideProps } from "next";
 import CollectionsList from "../../components/CollectionsList";
+import { useState } from "react";
 
-const Collections = () => {
-  return <CollectionsList />
-}
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const collections: Collection[] = await prisma.collection.findMany();
+  return { props: { initialCollections: collections } };
+};
 
-export default Collections
+const Collections = ({ initialCollections }) => {
+  const [collections, setCollections] =
+    useState<Collection[]>(initialCollections);
+  console.log(collections);
+  return (
+    <>
+      <h2 className="text-2xl font-bold text-gray-900">Collections</h2>
+      <CollectionsList collections={collections} />
+    </>
+  );
+};
+
+export default Collections;
